24ed6f60b7586762b8b159cc4a9bccb9dbf439c6
MhmdFayedh <mhmdfayedh@gmail.com>
2023-08-06 09:12:32 +0300

    Create PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Post;
+use App\Models\PostView;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::query()
+        ->where('active', '=', true)
+        ->whereDate('publish_at', '<=', Carbon::now())
+        ->orderBy('publish_at', 'desc')
+        ->get();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show($id, Request $request)
+    {
+        $post = Post::query()
+        ->where('active', '=', true)
+        ->where('id', '=', $id)
+        ->whereDate('publish_at', '<=', Carbon::now())
+        ->limit(1)
+        ->first();
+
+        if(!$post){
+            return redirect('/');
+        }
+
+        if( ! $request->cookie('considered')) {
+            PostView::create([
+                'ip_address' => $request->ip(),
+                'user_agent' => $request->userAgent(),
+                'user_host' => $request->getHost(),
+                'post_id' => $post->id,
+            ]);
+        }
+
+        return response(view('posts.show', compact('post')))->withCookie('considered', 'true', 60);
+    }
+}
