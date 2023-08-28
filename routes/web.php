0d294c25d6de16a724739c155a0b9719febd7ef4
MhmdFayedh <mhmdfayedh@gmail.com>
2023-08-28 21:00:36 +0300

    Group works by resource only index and show mehtods

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\WorkController;
+use App\Http\Controllers\{HomeController, PostController, WorkController};
 
 
 Route::get('/', HomeController::class);
 
 Route::resource('posts', PostController::class)->only([
+    'index',
+    'show'
+]);
+
+Route::resource('works', WorkController::class)->only([
     'index',
     'show'
 ]);
